e8f5820646dfab0cf4201e0b233aab21be678a2b
Dan Kaufman <daniel.kaufman_1@yahoo.ca>
2025-08-27 21:48:19 -0400

    added install script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!usr/bin/env bash
+set -e 
+
+REPO="DanKaufmanDev/QwikBootCLI"
+BINARY="qwikboot"
+INSTALL_DIR="/usr/local/bin"
+
+echo "Installing $BINARY..."
+
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+
+if [[ $OS == "darwin" ]]; then
+    TARGET="x86_64-apple-darwin"
+elif [[ $OS == "linux" ]]; then
+    TARGET="x86_64-unknown-linux-gnu"
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
+
+URL="https://github.com/repos/$REPO/releases/latest \
+    | grep "browser_download_url.*$TARGET" \
+    | cut -d'"' -f 4 \
+
+echo "Downloading $BINARY from $URL"
+curl -L "$URL" -o /tmp/$BINARY.tar.gz
+tar -xzf /tmp/$BINARY.tar.gz -C /tmp
+
+sudo cp /tmp/$BINARY $INSTALL_DIR/$BINARY
+sudo chmod +x $INSTALL_DIR/$BINARY
+
+echo "QwikBoot CLI installed successfully!"
